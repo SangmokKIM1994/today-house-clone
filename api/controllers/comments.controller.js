ea0e5b2c2d8f7ae67c99040d0bd1e1dd99c6a8a5
Ryeonjoo <syeonjoo@naver.com>
2023-03-03 20:04:05 +0900

    fix: 3계층 CRUD 기본 작성

--- api/controllers/comments.controller.js
+++ api/controllers/comments.controller.js
@@ -1,13 +1,55 @@
 const { error } = require("../error/error.js");
 const CommentsService = require("../services/comments.service.js");
 
-module.exports = class CommentsController {
+class CommentsController {
   //댓글 작성
-  createComment = async (req, res, next) => {};
+  createComment = async (req, res, next) => {
+    const { userId } = res.locals.user;
+    const { postId } = req.params;
+    const { comment } = req.body;
+
+    const result = await this.CommentsService.createComment({
+      userId,
+      postId,
+      comment,
+    });
+    return res.status(201).json({ message: "댓글이 생성되었습니다." });
+  };
+
   //댓글 조회
-  getComments = async (req, res, next) => {};
+  getComments = async (req, res, next) => {
+    const { postId } = req.parmas;
+    const result = await this.CommentsService.getComments({
+      postId,
+    });
+    return res.status(200).json({ data: result });
+  };
+
   //댓글 수정
-  editComments = async (req, res, next) => {};
+  editComments = async (req, res, next) => {
+    const { userId } = res.locals.user;
+    const { commentId } = req.params;
+    const { comment } = req.body;
+
+    const result = await this.CommentsService.editComments({
+      userId,
+      commentId,
+      comment,
+    });
+    return res.status(200).json({ message: "댓글이 수정되었습니다." });
+  };
+
   //댓글 삭제
-  deleteComment = async (req, res, next) => {};
-};
+  deleteComment = async (req, res, next) => {
+    const { userId } = res.locals.user;
+    const { commentId } = res.params;
+
+    const result = await this.CommentsService.deleteComment({
+      userId,
+      commentId,
+    });
+    return res.status(200).json({ message: "댓글이 삭제되었습니다." });
+  };
+}
+
+module.exports = CommentsController;
