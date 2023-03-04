7b2a425458f74bf88fb5d88b644390def3868af0
Leeseungwoon <seong3546@gmail.com>
2023-03-05 00:27:59 +0900

    fix: joi.helper.js 파일 추가 및 users.Service 코드 수정

---
+++ api/helpers/joi.helper.js
@@ -0,0 +1,63 @@
+const Joi = require("joi");
+const { makeError } = require("../error");
+
+const JoiHelper = {
+  //Users
+  signUpCheck: async (req, res, next) => {
+    const check = Joi.object().keys({
+      email: Joi.string()
+        .email()
+        .required()
+        .error(new makeError("email 형식에 맞춰서 입력 바랍니다.")),
+
+      nickname: Joi.string()
+        .required()
+        .regex(/^([a-zA-Z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣]){2,15}$/)
+        .error(
+          new makeError("닉네임은 특수문자 제외 2~15자만 입력이 가능합니다.")
+        ),
+
+      password: Joi.string()
+        .required()
+        .regex(/^[a-zA-Z0-9]{8,}$/)
+        .error(
+          new makeError("비밀번호는 영문, 숫자를 포함한 8자리 이상 조합입니다.")
+        ),
+
+      confirm: Joi.string()
+        .required()
+        .Joi.ref("password")
+        .error(new makeError("비밀번호 확인이 일치하지 않습니다.")),
+    });
+
+    try {
+      await check.validateAsync(req.body);
+    } catch (error) {
+      next(error);
+    }
+    next();
+  },
+
+  loginCheck: async (req, res, next) => {
+    const check = Joi.object().keys({
+      email: Joi.String()
+        .required()
+        .email()
+        .error(new makeError("email 형식에 맞춰서 입력 바랍니다.")),
+
+      password: Joi.String()
+        .required()
+        .regex(/^([a-zA-Z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣]){2,15}$/)
+        .error("비밀번호는 영문, 숫자를 포함한 8자리 이상 조합입니다."),
+    });
+
+    try {
+      await check.validateAsync(req.body);
+    } catch (error) {
+      next(error);
+    }
+    next();
+  },
+};
+
+module.exports = JoiHelper;
--- api/services/user.service.js
+++ api/services/user.service.js
@@ -7,16 +7,11 @@ class UsersService {
     this.usersRepository = new UsersRepository();
   }
 
-  signupService = async ({ email, password, confirm, nickname }) => {
+  signupService = async ({ email, password, nickname }) => {
     const emailExistUser = await this.usersRepository.loginRepo({ email });
     const nickNameExistUser = await this.usersRepository.nickNameRepo({
       nickname,
     });
-
-    const re_email =
-      /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+).(\.[0-9a-zA-Z_-]+){1,3}$/;
-    const re_nickname = /^([a-zA-Z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣]){2,15}$/;
-    const re_password = /^[a-zA-Z0-9]{8,}$/;
 
     if (emailExistUser) {
       throw new makeError({
@@ -27,30 +22,6 @@ class UsersService {
     if (nickNameExistUser) {
       throw new makeError({
         message: "중복 된 닉네임이 존재합니다.",
-        code: 400,
-      });
-    }
-    if (!re_email.test(email)) {
-      throw new makeError({
-        message: "email 형식에 맞춰서 입력 바랍니다.",
-        code: 400,
-      });
-    }
-    if (!re_nickname.test(nickname)) {
-      throw new makeError({
-        message: "닉네임은 특수문자 제외 2~15자만 입력이 가능합니다. ",
-        code: 400,
-      });
-    }
-    if (!re_password.test(password)) {
-      throw new makeError({
-        message: "비밀번호는 영문, 숫자를 포함한 8자리 이상 조합입니다.",
-        code: 400,
-      });
-    }
-    if (password !== confirm) {
-      throw new makeError({
-        message: "비밀번호 확인이 일치하지 않습니다.",
         code: 400,
       });
     }
@@ -69,37 +40,16 @@ class UsersService {
 
   loginService = async ({ email, password }) => {
     const isExistUser = await this.usersRepository.loginRepo({ email });
-
-    const re_email =
-      /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+).(\.[0-9a-zA-Z_-]+){1,3}$/;
-    const re_password = /^[a-zA-Z0-9]{8,}$/;
-
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      isExistUser.password
+    );
     if (!isExistUser) {
       throw new makeError({
         message: "email이 존재하지 않습니다.",
         code: 400,
       });
     }
-
-    if (!re_email.test(email)) {
-      throw new makeError({
-        message: "email 형식에 맞춰서 입력 바랍니다.",
-        code: 400,
-      });
-    }
-
-    if (!re_password.test(password)) {
-      throw new makeError({
-        message: "비밀번호는 영문, 숫자를 포함한 8자리 이상 조합입니다.",
-        code: 400,
-      });
-    }
-
-    const isPasswordCorrect = await bcrypt.compare(
-      password,
-      isExistUser.password
-    );
-
     if (!isPasswordCorrect) {
       throw new makeError({
         message: "비밀번호가 일치하지 않습니다.",
